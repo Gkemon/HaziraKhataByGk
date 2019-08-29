fa94e851a5bb2674e89f23c161e9f7a6de92c060
Emon <emon@field.buzz>
2019-08-29 17:32:37 +0600

    Add routine add dialog

--- app/src/main/java/com/Teachers/HaziraKhataByGk/Routine/RoutineInputDialog.java
+++ app/src/main/java/com/Teachers/HaziraKhataByGk/Routine/RoutineInputDialog.java
@@ -3,9 +3,12 @@ package com.Teachers.HaziraKhataByGk.Routine;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.Teachers.HaziraKhataByGk.Constant.Constant;
@@ -14,7 +17,27 @@ import com.Teachers.HaziraKhataByGk.HelperClassess.ViewUtils.ViewPagerAdapter;
 import com.Teachers.HaziraKhataByGk.R;
 import com.Teachers.HaziraKhataByGk.Widget.BaseFullScreenDialog;
 
+import butterknife.BindView;
+
 public class RoutineInputDialog extends BaseFullScreenDialog {
+
+
+    @BindView(R.id.rb_class_routine)
+    RadioButton rbClassRoutine;
+    @BindView(R.id.rb_exam_routine)
+    RadioButton rbExamRoutine;
+    @BindView(R.id.rb_admin_routine)
+    RadioButton rbAdminRoutine;
+    @BindView(R.id.et_subject)
+    EditText etSubject;
+    @BindView(R.id.et_room)
+    EditText etRoom;
+    @BindView(R.id.bt_from_time)
+    AppCompatButton btnFromTime;
+    @BindView(R.id.bt_to_time)
+    AppCompatButton btnToTime;
+    @BindView(R.id.weekdays)
+
 
 
     public static void showDialog(android.support.v4.app.FragmentManager manager) {
@@ -33,7 +56,6 @@ public class RoutineInputDialog extends BaseFullScreenDialog {
         super.onCreateView(inflater, container, savedInstanceState);
 
         View view = inflater.inflate(R.layout.dialog_add_routine_item, container, false);
-
 
 
         return view;
